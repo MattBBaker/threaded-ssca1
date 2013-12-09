3f6509b5a94b9f373b861ed8541d6a691d08a5e5
Matthew Baker <bakermb@ornl.gov>
2013-12-09 11:30:30 -0500

    New more scalable data generator, second try

--- gen_scal_data.c
+++ gen_scal_data.c
@@ -1,1 +1,101 @@
-gen_scal_data.c.old
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sort.h>
+#include <gen_scal_data.h>
+#include <parameters.h>
+
+char validations[2][3][32] =
+  {
+    {"ACDEFG*IDENTICAL*HIKLMN", "ACDEFG*MISQRMATCHES*HIKLMN", "ACDEFG*STARTGAPMIDSTEND*HIKLMN"},
+    {"MNLKIH*IDENTICAL*GFEDCA", "MNLKIH*MISRQMATCHES*GFEDCA", "MNLKIH*STARTMIDSTGAPEND*GFEDCA"}
+  };
+
+/* print the output */
+
+void verifyData(sim_matrix_t *simMatrix, seq_data_t *seqData)
+{
+  printf("\n");
+  printf("   Length of main sequence in codons: %i\n", seqData->mainLen);
+  printf("  Length of match sequence in codons: %i\n", seqData->matchLen);
+  printf("  Weight for exactly matching codons: %i\n", simMatrix->exact);
+  printf("           Weight for similar codons: %i\n", simMatrix->similar);
+  printf("        Weight for dissimilar codons: %i\n", simMatrix->dissimilar);
+  printf("              Penalty to start a gap: %i\n", simMatrix->gapStart);
+  printf("     Penalty for each codon in a gap: %i\n", simMatrix->gapExtend);
+}
+
+int *gen_indexes(int num_indexes, int rand_max, int min_seperation) {
+  int not_done;
+  int *indexes = malloc(sizeof(int) * num_indexes);
+  do{
+    not_done = 0;
+    for(int idx=0; idx < num_indexes; idx++) {
+      indexes[idx] = rand()%rand_max;
+    }
+    for(int idx=0; idx < num_indexes; idx++) {
+      for(int jdx=idx+1; jdx < num_indexes; jdx++) {
+	if(indexes[idx] - indexes[jdx] < min_seperation) not_done = 1;
+      }
+    }
+  } while(not_done);
+  return indexes;
+}
+
+void create_sequence(int *sequence, char validations[][32], int sequence_length, int num_validations, sim_matrix_t *simMatrix){
+  int total_length = sequence_length;
+  int end;
+  for(int idx=0; idx < num_validations; idx++){
+    total_length += strlen(validations[idx]);
+  }
+  int *indexes = gen_indexes(num_validations, total_length-32, 32);
+
+  for(int idx=0; idx < total_length; idx++) {
+    sequence[idx] = rand()%64;
+  }
+
+  for(int idx=0; idx < num_validations; idx++) {
+    end = strlen(validations[idx]);
+    for(int jdx=0; jdx < end; jdx++){
+      sequence[indexes[idx]+jdx] = simMatrix->encode[(int)validations[idx][jdx]];
+    }
+  }
+}
+
+seq_data_t *gen_scal_data( sim_matrix_t *simMatrix, int mainLen, int matchLen) {
+  seq_data_t *new_scal_data = (seq_data_t *)malloc(sizeof(seq_data_t));
+  int validation_length, validation_size=0;
+
+  memset(new_scal_data, '\0', sizeof(seq_data_t));
+  for(int idx=0; idx < 2; idx++){
+    for(int jdx=0; jdx < 3; jdx++){
+      validation_length = strlen(validations[idx][jdx]);
+      validation_size += validation_length;
+      if(validation_length > new_scal_data->maxValidation)
+	new_scal_data->maxValidation = validation_length;
+    }
+  }  
+  validation_size /= 2;
+
+  new_scal_data->maxValidation -= 12;
+  new_scal_data->mainLen = mainLen + validation_size;
+  new_scal_data->matchLen = matchLen + validation_size;
+  new_scal_data->main = malloc(sizeof(codon_t)*(mainLen + validation_size));
+  new_scal_data->match = malloc(sizeof(codon_t)*(matchLen + validation_size));
+  
+
+  int *gen_sequences[2] = {new_scal_data->main, new_scal_data->match};
+  int seq_lengths[2] = {mainLen, matchLen};
+
+  for(int idx=0; idx < 2; idx++){
+    create_sequence(gen_sequences[idx], validations[idx], seq_lengths[idx], 3, simMatrix);
+  }
+
+  return new_scal_data;
+}
+
+void release_scal_data(seq_data_t *doomed_seq) {
+  free((void *)(doomed_seq->main));
+  free((void *)(doomed_seq->match));
+  free((void *)doomed_seq);
+}
